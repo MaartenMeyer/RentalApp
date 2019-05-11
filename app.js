79f3b31d49d5a80aee448b456513dadf95432c93
Maarten <maartenmeyer8@gmail.com>
2019-05-11 17:42:13 +0200

    Git test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const authenticationRoutes = require('./src/routes/authentication.routes');
 const logger = require('./src/config/config').logger
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 
