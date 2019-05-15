668d377e09ade3154dd1d73b5b54cc0f5990661b
Maarten <maartenmeyer8@gmail.com>
2019-05-15 19:21:45 +0200

    Changed to remote database and added ci travis

--- src/config/config.js
+++ src/config/config.js
@@ -14,15 +14,15 @@ module.exports = {
   }),
 
   dbconfig: {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    //port: 1443,
-    //driver: 'msnodesql',
-    //connectionTimeout: 1500,
-    //options: {
-    //  encrypt: false
-    //}
+    user: 'progr4',
+    password: 'password123',
+    server: 'aei-sql.avans.nl',
+    database: 'Prog4-Eindopdracht1',
+    port: 1443,
+    driver: 'msnodesql',
+    connectionTimeout: 1500,
+    options: {
+      encrypt: false
+    }
   }
 }
